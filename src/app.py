cb10df5bc9d55499ae4c77f6c802e31fa25c5219
Campeones1950 <168790271+Campeones1950@users.noreply.github.com>
2024-11-08 00:03:54 +0000

    Entrega ejercicio API Family Jackson

--- src/app.py
+++ src/app.py
@@ -30,13 +30,68 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body =  members
 
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def post_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({ 'msg': 'Debes enviar información en el body'}),400
+    if 'first_name' not in body:
+        return jsonify({'msg': 'El campo first_name es obligatorio'}),400
+    if 'age' not in body:
+        return jsonify({'msg': 'El campo age es obligatorio'}),400
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg': 'El campo lucky_numbers es obligatorio'}),400
+    
+    new_member = {
+        'id': body.get( 'id', jackson_family._generateId()),
+        'first_name': body['first_name'],
+        'last_name' : jackson_family.last_name, 
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+    added_member = jackson_family.add_member(new_member)
+    return jsonify({"msg": "ok", "member": added_member}), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify({
+            'id': member['id'],
+            'first_name': member['first_name'],
+            'age': member['age'],
+            'lucky_numbers': member['lucky_numbers']
+        }),200
+    else:
+        return jsonify({'msg':f'No se encontró el miembro con id {member_id}'}), 400   
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])    
+def delete_member(member_id):
+    success = jackson_family.delete_member(member_id)
+    if success:
+        return jsonify({'done': True}),200
+    else:
+        return jsonify({'msg':f'No se encontró el miembro con id {member_id}'}), 404
+    
+# Solución para la solicitud de componente familiar con id fijo 3443
+@app.route('/member/<int:member_id>', methods=['PUT'])    
+def update_family_member(member_id):
+    new_data = request.get_json(silent =True)
+    if not new_data:
+        return jsonify({"msg": "Debes enviar los datos para actualizar en el Body"}),400
+    
+    updated_member = jackson_family.update_member(member_id, new_data)
+    if updated_member:
+        return jsonify({ "done": "Miembro actualizado", "member" :updated_member}),200
+    else:
+        return jsonify({"msg": "No se encontró el miembro con el id proporcionado"}), 404
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,30 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+         
+            {'id': self._generateId(),
+             'first_name':'John',
+             'last_name': self.last_name,
+             'age': '33',
+              'lucky_numbers': [7, 13, 22]
+
+            },
+            {'id': self._generateId(),
+             'first_name':'Jane',
+             'last_name': self.last_name,
+             'age': '35',
+              'lucky_numbers': [10, 14, 3]
+
+            },
+            {'id': self._generateId(),
+             'first_name':'Jimmy',
+             'last_name': self.last_name,
+             'age': '5',
+              'lucky_numbers': [1]
+
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +44,31 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+       self._members.append(member)
+       return member
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
-
+        for i, member in enumerate(self._members):
+           if member ['id'] == id:
+                del self._members[i]
+                return True
+        return False   
+      
+    
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
+            
+    def update_member(self, id, new_data):
+        for member in self._members:
+            if member['id'] == id:
+                member.update(new_data)
+                return member
+        return None
+     
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
